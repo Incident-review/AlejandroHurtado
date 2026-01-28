2be4375c0a1124c93767ad316bc4cc1a565cc416
IncidentReview <incidentreview.viktor@gmail.com>
2026-01-28 18:31:42 +0100

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,13 @@ import App from './App';
 import './index.css';
 import theme from './theme';
 import './i18n/config'; 
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <App />
+      <Analytics />
     </ChakraProvider>
   </React.StrictMode>
 );
-
